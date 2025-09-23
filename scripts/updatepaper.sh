160bc7e5197bc7cd15e372a0c31a38b39f24830d
Project516 <138796702+Project516@users.noreply.github.com>
2025-09-23 09:31:23 -0500

    Update updatepaper.sh

--- scripts/updatepaper.sh
+++ scripts/updatepaper.sh
@@ -8,3 +8,7 @@ pkill -f 'server.jar'
 rm -f server.jar
 # Download the server.jar file
 curl -s "https://project516.github.io/RaspberryPiGuide/scripts/installpaper.sh" | bash
+# Remove Geyser
+rm -f plugins/geyser.jar
+# Redownload Geyser
+curl -s -L -o plugins/geyser.jar https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot
