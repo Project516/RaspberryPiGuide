dbe06bafbe4d675340ca3c887ec9907f94c95c7b
Project516 <138796702+Project516@users.noreply.github.com>
2025-10-26 17:40:35 -0500

    Update updatepaper.sh

--- scripts/updatepaper.sh
+++ scripts/updatepaper.sh
@@ -18,8 +18,8 @@ rm -f plugins/floodgate.jar
 # Redownload Floodgate
 curl -s -L -o plugins/floodgate.jar https://download.geysermc.org/v2/projects/floodgate/versions/latest/builds/latest/downloads/spigot
 rm -f plugins/viaversion.jar
-curl -s -L -o plugins/viaversion.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.4.2/PAPER/ViaVersion-5.4.2.jar
+curl -s -L -o plugins/viaversion.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaVersion/versions/5.5.1/PAPER/ViaVersion-5.5.1.jar
 rm -f plugins/viabackwards.jar
-curl -s -L -o plugins/viabackwards.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaBackwards/versions/5.4.2/PAPER/ViaBackwards-5.4.2.jar
+curl -s -L -o plugins/viabackwards.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaBackwards/versions/5.5.1/PAPER/ViaBackwards-5.5.1.jar
 rm -f plugins/viarewind.jar
-curl -s -L -o plugins/viarewind.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaRewind/versions/4.0.9/PAPER/ViaRewind-4.0.9.jar
+curl -s -L -o plugins/viarewind.jar https://hangarcdn.papermc.io/plugins/ViaVersion/ViaRewind/versions/4.0.11/PAPER/ViaRewind-4.0.11.jar
